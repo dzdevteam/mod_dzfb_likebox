ebe9540526b6e1e311b79feb015b5696ac3c111b
Lê Hoàng Phương <herophuong93@gmail.com>
2013-07-25 15:00:22 +0700

    add base source from foobar

---
+++ tmpl/default.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @version     1.0.0
+ * @package     mod_dzfb_likebox
+ * @copyright   Copyright (C) 2013. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @author      DZ Team <support@dezign.vn> - dezign.vn
+ */
+ 
+// no direct access
+defined('_JEXEC') or die;
+?>
+<div class="dzfb_likebox-module<?php echo $moduleclass_sfx; ?>">
+</div>
